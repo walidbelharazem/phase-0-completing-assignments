0a9daf6b4341ad15b3c538b97e129d42cf6b2919
walidbelharazem <walidbelharazem@gmail.com>
2023-07-02 00:03:44 +0200

    Donne

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,7 +4,7 @@ var chai = require( 'chai' );
 chai.use( require( 'chai-fs' ) );
 
 describe( 'This assignment', () => {
-  it( 'has been correctly cloned to your local environment', () => {
+  it( 'has been correctly cloned to my local environment', () => {
     chai.assert.isDirectory( './.git', 'no ".git" folder was found within "welcome-completing-assignment". Use "git init" to create one' );
   } )
 } )
